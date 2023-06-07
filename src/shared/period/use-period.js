e64aff547f5d2324ce0884865ca41e7a11d8fb4f
Alex Tumwesigye <atumwesigye@gmail.com>
2023-06-07 17:29:17 +0300

    Support ethiopian calendar

--- src/shared/period/use-period.js
+++ src/shared/period/use-period.js
@@ -1,19 +1,31 @@
 import { createFixedPeriodFromPeriodId } from '@dhis2/multi-calendar-dates'
-import { useMemo } from 'react'
+import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
+const queryKey = [`/system/info`]
+
+const queryOpts = {
+    refetchOnMount: false,
+    select: selectorFunction,
+    staleTime: 24 * 60 * 60 * 1000,
+}
+
+const calendar = 'gregory';
 export default function usePeriod(periodId) {
     // @TODO(calendar)
-    const calendar = 'gregory'
+    const { data } = useQuery(queryKey, queryOpts);
+    
     return useMemo(() => {
+        const currentCalendar = (data?.calendar==='ethiopian'?'ethiopic':data?.calendar)??calendar;
         if (!periodId) {
             return null
         }
 
         try {
-            return createFixedPeriodFromPeriodId({ periodId, calendar })
+            return createFixedPeriodFromPeriodId({ periodId, currentCalendar })
         } catch (e) {
             console.error(e)
             return null
         }
-    }, [periodId])
+    }, [periodId,data?.calendar])
 }
