95cdd0a0cca3d5fc7b57e1d3f32e7e43461a13d2
Alex Tumwesigye <atumwesigye@gmail.com>
2023-06-10 01:45:51 +0300

    Fix ET calendar date

--- src/shared/date/format-js-date-to-date-string.js
+++ src/shared/date/format-js-date-to-date-string.js
@@ -1,4 +1,6 @@
-export default function formatJsDateToDateString(date) {
+export default function formatJsDateToDateString(dateString) {
+    console.log("date:",dateString);
+    const date = new Date(dateString);
     const yyyy = date.getFullYear()
     const mm = String(date.getMonth() + 1).padStart(2, '0')
     const dd = String(date.getDate()).padStart(2, '0')
