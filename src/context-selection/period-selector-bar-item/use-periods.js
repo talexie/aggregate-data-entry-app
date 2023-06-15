dcb691c46062775356b0908fd9c07699e91c4307
Alex Tumwesigye <atumwesigye@gmail.com>
2023-06-15 17:35:01 +0300

    Fix dates

--- src/context-selection/period-selector-bar-item/use-periods.js
+++ src/context-selection/period-selector-bar-item/use-periods.js
@@ -40,7 +40,6 @@ export default function usePeriods({
         // + 1 so we include 1970 as well
         yearsCount: yearForGenerating - 1970 + 1,
     }),[calendar,periodType, yearForGenerating,endsBefore,locale]);
-    console.log("YYY::", generateFixedPeriodsPayload);
     useEffect(()=>{
         setIsYearlyPeriodType(yearlyFixedPeriodTypes.includes(periodType));
     },[periodType]);
@@ -69,7 +68,7 @@ export default function usePeriods({
             return periods
         }
         const [lastPeriodOfPrevYear] = generateFixedPeriods({
-            ...generateFixedPeriodsPayload,
+            ...generateFixedPeriodsPayload(),
             year: yearForGenerating - 1,
         }).slice(-1)
         const [firstPeriodNextYear] = generateFixedPeriods({
